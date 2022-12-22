b07d9a87641bb8cb9be05b48437c50f39671a9ed
AhmedQ2004 <121233841+AhmedQ2004@users.noreply.github.com>
2022-12-22 15:54:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the beginning: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the end: ");
+            double n2 = double.Parse(Console.ReadLine());
+            if (n1 > n2)
+            {
+                Console.WriteLine("please restart the program and enter numbers correctly");
+            }
+            else
+            { 
+            if (n1 < 2)
+            {
+                n1 = 2;
+            }
+                for (double i = n1; i <= n2; i++)
+                {
+                    double sum = 0;
+                    for (double a = 1; a < i; a++)
+                    {
+                        if (i % a == 0)
+                        {
+                            sum += a;
+                        }
+                    }
+                    if (sum == i)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
